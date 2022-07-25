27fa166fc0432b9ffc5352ee687d03a5412ab75d
Ho-Chun Huang <Ho-Chun.Huang@noaa.gov>
2022-07-25 13:56:19 +0000

    add module load in jobcard

--- scripts/get_nco_hpss2ndir_nam_bufr.sh
+++ scripts/get_nco_hpss2ndir_nam_bufr.sh
@@ -69,6 +69,12 @@ cat > ${batch_script} << EOF
 #PBS -A AQM-DEV
 #PBS -l walltime=${task_cpu}
 #PBS -l debug=true
+module load envvar/1.0
+module load PrgEnv-intel/8.2.0
+module load intel/19.1.3.304
+module load craype/2.7.8
+module load cray-mpich/8.1.9
+
 ##
 ##  Provide fix date daily Hysplit data processing
 ##
