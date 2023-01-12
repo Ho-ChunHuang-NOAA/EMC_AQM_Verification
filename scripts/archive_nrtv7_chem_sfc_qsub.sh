354296c80c961780c52060ba0bc94254de6c28d9
Ho-Chun Huang <Ho-Chun.Huang@noaa.gov>
2023-01-12 19:52:46 +0000

    incremental changes

--- scripts/archive_nrtv7_chem_sfc_qsub.sh
+++ scripts/archive_nrtv7_chem_sfc_qsub.sh
@@ -23,7 +23,11 @@ fi
 echo "out_envir=${out_envir}"
 expid=`echo ${out_envir} | cut -c4-6`
 
+## send output to v70c43"
 out_envir=${in_envir}
+out_envir=v70c43
+
+
 data_in=/lfs/h2/emc/ptmp/ho-chun.huang/data/RRFSCMAQ/${in_envir}
 data_in=/lfs/h2/emc/ptmp/jianping.huang/emc.para/com/aqm/v7.0/aqm.v7.0.${expid}
 data_in=/lfs/h2/emc/aqmtemp/para/com/aqm/v7.0
@@ -45,6 +49,9 @@ if [ ! -d ${rundir} ]; then mkdir -p ${rundir}; fi
 hpssroot=/5year/NCEPDEV/emc-naqfc/Ho-Chun.Huang/RRFS_sfc_chem_met/${out_envir}
 outdir=/lfs/h2/emc/physics/noscrub/${USER}/rrfs_sfc_chem_met/${out_envir}
 if [ ! -d ${outdir} ]; then mkdir -p ${outdir}; fi
+bchpssroot=/5year/NCEPDEV/emc-naqfc/Ho-Chun.Huang/RRFS_sfc_chem_met/${out_envir}_bc
+bcoutdir=/lfs/h2/emc/physics/noscrub/${USER}/rrfs_sfc_chem_met/${out_envir}_bc
+if [ ! -d ${bcoutdir} ]; then mkdir -p ${bcoutdir}; fi
 
 NOW=${FIRSTDAY}
 YY0=`echo ${NOW} | cut -c1-4`
@@ -91,9 +98,11 @@ module load cray-mpich/8.1.9
     cd ${working_dir}
     NOW=${NOW}
     USER=${USER}
+    bchpssroot=${bchpssroot}/${YY}/${YM}
     hpssroot=${hpssroot}/${YY}/${YM}
     working_dir=${working_dir}
     outdir=${outdir}
+    bcoutdir=${bcoutdir}
     data_in=${data_in}
     out_envir=${out_envir}
     expid=${expid}
@@ -103,7 +112,8 @@ if [ -s ${batch_script}.add ]; then /bin/rm -f ${batch_script}.add; fi
 cat > ${batch_script}.add << 'EOF'
     declare -a cyc=( 06 12 )
     odir=${outdir}/aqm.${NOW}
-    mkdir -p ${odir}
+    bcodir=${bcoutdir}/aqm.${NOW}
+    mkdir -p ${odir} ${bcodir}
     for i in "${cyc[@]}"; do
         idir=${data_in}/${expid}.${NOW}/${i}
 	if [ -d ${idir} ]; then
@@ -111,11 +121,11 @@ cat > ${batch_script}.add << 'EOF'
             cp -p ${idir}/aqm.t${i}z.met_sfc.*.nc ${odir}
 	    cpfile=${idir}/ozone.corrected.${NOW}.${i}z.nc
 	    if [ -s ${cpfile} ]; then
-	        cp -p ${cpfile} ${odir}
+	        cp -p ${cpfile} ${bcodir}
 	    fi
 	    cpfile=${idir}/pm2.5.corrected.${NOW}.${i}z.nc
 	    if [ -s ${cpfile} ]; then
-	        cp -p ${cpfile} ${odir}
+	        cp -p ${cpfile} ${bcodir}
 	    fi
 	    echo "${NOW} ${i}"
         else
@@ -124,9 +134,17 @@ cat > ${batch_script}.add << 'EOF'
     done
     cd ${outdir}
     if [ -d aqm.${NOW} ]; then
+	hsi mkdir -p ${hpssroot}
         htar -cf ${hpssroot}/aqm.${NOW}.tar aqm.${NOW} 
     else
         echo "Can not find ${outdir}/${out_envir}/aqm.${NOW}"	    
+    fi
+    cd ${bcoutdir}
+    if [ -d aqm.${NOW} ]; then
+	hsi mkdir -p ${bchpssroot}
+        htar -cf ${bchpssroot}/aqm.${NOW}.tar aqm.${NOW} 
+    else
+        echo "Can not find ${bcoutdir}/${out_envir}/aqm.${NOW}"	    
     fi
 exit
 EOF
